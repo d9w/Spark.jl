4ee31965e8ea689f1d422a3db0aa008e742b1662
Dennis Wilson <d9w@mit.edu>
2014-04-22 19:11:00 -0400

    barebones RDD definition

---
+++ src/RDD.jl
@@ -0,0 +1,17 @@
+type RDD
+  ID::Int64
+  partitions::Array[PID]
+  dependencies::Array[Int64]
+  history::Array[Record]
+end
+
+type PID
+  node::Int64
+  ID::Int64
+end
+
+type Record
+  operation::Function
+  argument::Any
+  sources::Dict{Int64,Array[(Int64,PID)]}
+end
