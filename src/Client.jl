d1e439a73c1b82edca58f38fac87d0748b2752bf
Tim Mickel <tmickel@mit.edu>
2014-05-02 02:12:15 -0400

    Completed master-to-worker RPC, some framework for other RPC

--- src/Client.jl
+++
@@ -1,8 +0,0 @@
-client = connect(6666)
-@async while true
-    write(STDOUT, readline(client))
-end
-while true
-    data = x -> x^2 + 2x - 4
-    println(client, serialize(client, data))
-end
--- src/Worker.jl
+++ src/Worker.jl
@@ -3,10 +3,11 @@ using JSON
 type Worker
     port::Int64
     active::Bool # can be turned off by an RPC
+    coworkers::Array{}
 end
 
 function Worker(port::Int64)
-    Worker(port,true)
+    Worker(port,true,{})
 end
 
 function start(worker::Worker)
@@ -17,7 +18,7 @@ function start(worker::Worker)
         while worker.active
             try
                 line = readline(sock)
-                handleline(worker, line)
+                handle(worker, line)
             catch e
                 showerror(STDERR, e)
                 break
@@ -26,9 +27,37 @@ function start(worker::Worker)
     end
 end
 
-function handleline(worker::Worker, line::ASCIIString)
+function handle(worker::Worker, line::ASCIIString)
+    # General format of a message:
+    # {:call => "funcname", :args => {anything}}
+    # this is dispatched to any function call - fine since we're assuming
+    # a private non-adversarial network.
     msg = JSON.parse(strip(line))
-    if "kill_worker" in keys(msg)
-        worker.active = false
+    if "call" in keys(msg)
+        eval(Expr(:call, symbol(msg["call"]), worker, msg["args"]))
     end
+end
+
+# Get RDD info from the master
+function getRDD(id)
+    # TODO - fill this in with real hostname/port
+    master = connect(hostname, port)
+    println(master, json({"id" => id}))
+    result = readline(master)
+    # TODO - will the RDD actually be JSON-encoded?  probably not
+    return JSON.parse(result)["rdd"]
+end
+
+# RPC functions here - called by the master, other workers
+function kill(worker::Worker, args::Dict)
+    worker.active = false
+end
+
+function wprint(worker::Worker, args::Dict)
+    println(args["str"])
+end
+
+# Receive a list of coworkers from the master
+function shareworkers(worker::Worker, args::Dict)
+    worker.coworkers = args["workers"]
 end
