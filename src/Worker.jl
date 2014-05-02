24a8cc99fdf86cc501f41ed375bbaf0d9b970c00
Tim Mickel <tmickel@mit.edu>
2014-05-01 21:55:07 -0400

    Worker killing, message handler

--- src/Worker.jl
+++ src/Worker.jl
@@ -1,24 +1,34 @@
+using JSON
+
 type Worker
-  port::Int64
-  active::Bool # can be turned off by an RPC
+    port::Int64
+    active::Bool # can be turned off by an RPC
 end
 
 function Worker(port::Int64)
-  Worker(port,true)
+    Worker(port,true)
 end
 
 function start(worker::Worker)
-  server = listen(IPv4(0), worker.port)
-  println("starting")
-  while worker.active
-    sock = accept(server)
-    try
-      line = deserialize(sock)
-      readline(sock)
-      println(line)
-    catch e
-      println(e)
-      break
+    server = listen(IPv4(0), worker.port)
+    println("starting")
+    while worker.active
+        sock = accept(server)
+        while worker.active
+            try
+                line = readline(sock)
+                handleline(worker, line)
+            catch e
+                showerror(STDERR, e)
+                break
+            end
+        end
     end
-  end
+end
+
+function handleline(worker::Worker, line::ASCIIString)
+    msg = JSON.parse(strip(line))
+    if "kill_worker" in keys(msg)
+        worker.active = false
+    end
 end
