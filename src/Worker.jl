2e0051c609ea3c96412fefacd5dc6d54590a11d2
Martin Martinez Rivera <martinmr@mit.edu>
2014-05-04 19:53:12 -0400

    First version of create_partition method

--- src/Worker.jl
+++ src/Worker.jl
@@ -35,3 +35,29 @@ function handle(worker::Worker, line::ASCIIString)
         eval(Expr(:call, symbol(msg["call"]), worker, msg["args"]))
     end
 end
+
+function create_partition(worker::Worker, rdd_id::Int64, partition_id::Int64, partition::Partition, 
+    source_rdds::Array{Array{PID}}, op_type::String, op_args::Dict{})
+    rdds::Array{Array{}} = Array(Array, 0)
+    for rdd in source_rdds
+        rdd_data:Array{}
+        for partition in rdd
+            partition_data::Array
+            
+            #attempt to get partition data. If partition cannot be found
+            #tell the master to rebuild it.
+            while true
+                #partition_data = get_partition(partition)
+                #if get_partition fails
+                #call rebuild_partition (or similar) method on the master
+                #need to update the partition data to reflect partition may be in a
+                #different worker after rebuilding it.
+                #else, break out the loop
+            end
+            rdd_data = vcat(rdd_data, partition_data)
+        end
+    end
+    
+    #handle_op executes operation dependant code and adds the new partition to worker.data
+    handle_op(worker, rdd_id, partition_id, partition, op_type, op_args, rdd_data)
+end
