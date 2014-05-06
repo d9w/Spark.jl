1c4ceca2d149f3c3a5d424c467e16b8908b69273
Martin Martinez Rivera <martinmr@mit.edu>
2014-05-05 23:03:07 -0400

    Removed create_partition method since it does not follow the Spark design

--- src/Worker.jl
+++ src/Worker.jl
@@ -35,30 +35,3 @@ function handle(worker::Worker, line::ASCIIString)
     end
     return r
 end
-
-function create_partition(worker::Worker, rdd_id::Int64, partition_id::Int64, partition::Partition, 
-    dependencies::Array{Array{PID}}, oper::Transformation)
-    rdds::Array{Array{Any}} = Array(Array{Any}, 0)
-    for rdd in dependencies
-        rdd_data:Array{Any}
-        for partition in rdd
-            partition_data::Array{Any}
-            
-            #attempt to get partition data. If partition cannot be found
-            #tell the master to rebuild it.
-            while true
-                #partition_data = get_partition(partition)
-                #if get_partition fails
-                #call rebuild_partition (or similar) method on the master
-                #need to update the partition data to reflect partition may be in a
-                #different worker after rebuilding it.
-                #else, break out the loop
-            end
-            rdd_data = vcat(rdd_data, partition_data)
-        end
-        append!(rdds, rdd_data)
-    end
-    
-    #handle_op executes operation dependant code and adds the new partition to worker.data
-    handle_op(worker, rdd_id, partition_id, partition, oper, rdd_data)
-end
