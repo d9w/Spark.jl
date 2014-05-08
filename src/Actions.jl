8b0760eed8d26f89ea6421b8a887751c822d2807
Tim Mickel <tmickel@mit.edu>
2014-05-07 22:46:40 -0400

    Adds Collect Action

--- src/Actions.jl
+++ src/Actions.jl
@@ -15,8 +15,19 @@ function count(worker::Worker, rdd::WorkerRDD, part_id::Int64, args::Dict)
     return length(collect(keys(rdd.partitions[part_id].data)))
 end
 
-function collect(worker::Worker, rdd::WorkerRDD, args::Dict{Any, Any})
-    return None
+function collect(master::Master, rdd::RDD)
+    op = Action("collect", Dict())
+    results = {}
+    results = cat(1, results, doop(master, rdd, op))
+    return results
+end
+
+function collect(worker::Worker, rdd::WorkerRDD, part_id::Int64, args::Dict)
+    results = {}
+    for key in keys(rdd.partitions[part_id].data)
+        push!(results, (key, rdd.partitions[part_id].data[key]))
+    end
+    return results
 end
 
 function reduce(worker::Worker, rdd::WorkerRDD, args::Dict{Any, Any})
@@ -26,8 +37,3 @@ end
 function lookup(worker::Worker, rdd::WorkerRDD, args::Dict{Any, Any})
     return None
 end
-
-function save(worker::Worker, rdd::WorkerRDD, args::Dict{Any, Any})
-    return None
-end
-
