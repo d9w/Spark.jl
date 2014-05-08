6e7ed9398a505016fac6ce57048bce9093c7652a
Nicole Power <npower@mit.edu>
2014-05-08 01:47:59 -0400

    more tests

--- test/op_test.jl
+++ test/op_test.jl
@@ -1,6 +1,24 @@
 using Spark
 
 # using master from setup.jl
+
+function input_test(master::Master)
+    rdd = Spark.input(master, "RDDA.txt", "test_reader")
+    @assert rdd != false
+    @assert length(keys(rdd.partitions)) == 3
+end
+
+function collect_test(master::Master)
+    rdd = Spark.input(master, "RDDA.txt", "int_reader")
+    collection = Spark.collect(master, rdd)
+    for kv in collection
+        @assert kv[1] <= 10
+        @assert length(kv[2]) == 1
+        @assert kv[2][1] <= 10
+    end
+    @assert length(collection) == 10
+end
+
 function count_test(master::Master)
     rdd = Spark.input(master, "RDDA.txt", "test_reader") 
     count = Spark.count(master, rdd)
@@ -13,6 +31,16 @@ function lookup_test(master::Master)
     @assert val == "3"
 end
 
+function partition_by_test(master::Master)
+    rdd = Spark.input(master, "RDDA.txt", "int_reader")
+    collection = Spark.collect(master, rdd)
+    partitioned_rdd = Spark.partition_by(master, rdd, Spark.HashPartitioner())
+    @assert Spark.count(master, rdd) == Spark.count(master, partitioned_rdd)
+    for kv in collection
+        @assert kv[2] == Spark.lookup(master, partitioned_rdd, kv[1])
+    end
+end
+
 function filter_test(master::Master)
     rdd = Spark.input(master, "RDDA.txt", "int_reader")
     filtered = Spark.filter(master, rdd, "number_filter")
@@ -20,4 +48,13 @@ function filter_test(master::Master)
     val = Spark.lookup(master, filtered, 7)
     @assert count == 5
     @assert val == false
+end
+
+function map_test(master::Master)
+end
+
+function group_by_key_test(master::Master)
+end
+
+function join_test(master::Master)
 end
