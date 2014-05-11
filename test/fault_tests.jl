efd8931d89022b3cf4c71e4cdc031e0998387b4e
Dennis Wilson <d9w@mit.edu>
2014-05-11 02:26:16 -0400

    join test failing

--- test/fault_tests.jl
+++ test/fault_tests.jl
@@ -15,3 +15,18 @@ function basic_disc_fault(master::Spark.Master)
         @assert 2*(kv[2][1]) == values[1]
     end
 end
+
+function join_disc_fault(master::Spark.Master)
+    rdd_a = Spark.input(master, "RDDA.txt", "int_reader")
+    rdd_b = Spark.input(master, "RDDA.txt", "int_reader")
+    joined_rdd = Spark.join(master, rdd_a, rdd_b)
+    master.workers[1].active = false
+    mapped_rdd = Spark.map(master, joined_rdd, "double_map")
+    @assert mapped_rdd == false
+    new_joined_rdd = Spark.join(master, rdd_a, rdd_b)
+    joined_collection = Spark.collect(master, new_joined_rdd)
+    @assert length(joined_collection) == 10
+    for kv in joined_collection
+        @assert length(kv[2]) == 2
+    end
+end
